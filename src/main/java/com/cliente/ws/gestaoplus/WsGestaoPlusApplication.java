1bf9e8c62697483f8bd17ec6e45a206e3d45fac0
Murilo <munarimurilo441@gmail.com>
2024-05-07 12:05:32 -0300

    Initial commit

---
+++ src/main/java/com/cliente/ws/gestaoplus/WsGestaoPlusApplication.java
@@ -0,0 +1,13 @@
+package com.cliente.ws.gestaoplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsGestaoPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsGestaoPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cliente/ws/gestaoplus/WsGestaoPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cliente.ws.gestaoplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsGestaoPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
