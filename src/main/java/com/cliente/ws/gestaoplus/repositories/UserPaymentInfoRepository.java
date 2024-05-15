47647f947dbb5244bb99ad9d90a114ce1c4dd39b
Murilo <munarimurilo441@gmail.com>
2024-05-14 23:10:40 -0300

    UserType e variantes criados

--- src/main/java/com/cliente/ws/gestaoplus/repositories/UserPaymentInfoRepository.java
+++ src/main/java/com/cliente/ws/gestaoplus/repositories/UserPaymentInfoRepository.java
@@ -1,6 +1,7 @@
 package com.cliente.ws.gestaoplus.repositories;
 
+import com.cliente.ws.gestaoplus.model.UserPaymentInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserPaymentInfoRepository extends JpaRepository<UserPaymentInfoRepository, Long> {
+public interface UserPaymentInfoRepository extends JpaRepository<UserPaymentInfo, Long> {
 }
